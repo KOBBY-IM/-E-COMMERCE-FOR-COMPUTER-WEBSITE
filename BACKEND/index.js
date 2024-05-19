2ab1c9caaf74edbff4b6abdd584a168c5a2a853e
KOBBY <nyarkotheo2@gmail.com>
2024-05-19 11:20:44 +0000

    created server and database connection with mongo atlas for the project database

---
+++ BACKEND/index.js
@@ -0,0 +1,37 @@
+const PORT = process.env.PORT
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT, (error) => {
+    if (!error) {
+        console.log(`listening on port ${PORT}`);
+    }
+    else {
+        console.log(`Error: `+error);
+    }
+})
+
+
+// mongodb connection
+mongoose.connect(process.env.MONGO_COMM)
+   .then(() => {
+        console.log("connected to mongodb");
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+
+app.get("/", (req, res) => {
+res.send("e-commerce app backend started");
+})
